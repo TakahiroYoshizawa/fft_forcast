00a615f650139dc63a274931fcd3181c3446a3c3
TakahiroYoshizawa <takataku.shako.11326@gmail.com>
2018-05-03 22:41:03 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='you can get forecast, you have to do only input csv file.',
+    author='TakahiroYoshizawa',
+    license='MIT',
+)
